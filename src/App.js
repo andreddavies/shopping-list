d7c85acf3f53611f18eebf48296ccc453aac8ebc
Andre <andreddavies@hotmail.com>
2021-10-25 13:31:57 -0300

    Implementing material UI theme provider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import "./App.css";
+import { MuiThemeProvider } from "@material-ui/core";
+import { createTheme } from "@material-ui/core/styles";
+
+import Header from "./components/Header";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#506dce",
+      main: "#00439c",
+      dark: "#001e6d",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#484848",
+      main: "#212121",
+      dark: "#000",
+      contrastText: "#fff",
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <h1>Test</h1>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <Header />
+      </div>
+    </MuiThemeProvider>
   );
 }
 
