8adaaab80b2d5a40189a96c0c12d540d794d2b77
Andre <andreddavies@hotmail.com>
2021-10-25 13:31:25 -0300

    Creating Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import './Header.css';
+import React from 'react';
+import { AppBar } from '@material-ui/core';
+
+const Header = () => {
+    return (
+        <div>
+            <AppBar position="static" color="primary">
+                <div className="header">
+                    <h2 className="title">Lista de Compras</h2>
+                </div>
+            </AppBar>
+        </div>
+    );
+};
+
+export default Header;
