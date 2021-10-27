bdd94918c5b16b3323447b113af57f5f454068bc
Andre <andreddavies@hotmail.com>
2021-10-27 13:50:54 -0300

    Creating List item and list footer components

---
+++ src/components/Main/List/ListFooter/ListFooter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ListFooter = (props) => {
+    return (
+        <div className={props.setFooterClassName}>
+            <p>10/26/21</p>
+            <p>100.00</p>
+        </div>
+    );
+};
+
+export default ListFooter;
